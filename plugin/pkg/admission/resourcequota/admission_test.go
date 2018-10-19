f732804bd69d478f8ac9ae3db5c6d7d0b8063571
Peng Du <peng.du1@huawei.com>
2018-10-19 15:51:30 -0700

    fixed pod count issue per Vinay's PR comment

--- plugin/pkg/admission/resourcequota/admission_test.go
+++ plugin/pkg/admission/resourcequota/admission_test.go
@@ -199,7 +199,7 @@ func TestAdmissionIgnoresSubresources(t *testing.T) {
 	}
 }
 
-func SetupQuotaforCreateAndUpdate(usedCPU string, usedMemory string, stopCh chan struct{}) (*fake.Clientset, *QuotaAdmission) {
+func SetupQuotaforCreateAndUpdate(usedCPU string, usedMemory string, stopCh chan struct{}, usedPods int) (*fake.Clientset, *QuotaAdmission) {
 	resourceQuota := &api.ResourceQuota{
 		ObjectMeta: metav1.ObjectMeta{Name: "quota", Namespace: "test", ResourceVersion: "124"},
 		Status: api.ResourceQuotaStatus{
@@ -211,7 +211,7 @@ func SetupQuotaforCreateAndUpdate(usedCPU string, usedMemory string, stopCh chan
 			Used: api.ResourceList{
 				api.ResourceCPU:    resource.MustParse(usedCPU),
 				api.ResourceMemory: resource.MustParse(usedMemory),
-				api.ResourcePods:   resource.MustParse("3"),
+				api.ResourcePods:   resource.MustParse(strconv.Itoa(usedPods)),
 			},
 		},
 	}
@@ -238,14 +238,14 @@ func TestAdmitBelowQuotaLimitWithDecreasingResourceUpdate(t *testing.T) {
 	utilfeaturetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.VerticalScaling, true)
 	stopCh := make(chan struct{})
 	defer close(stopCh)
-	kubeClient, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh)
+	kubeClient, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh, 3)
 	oldPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("200m", "10Gi"), getResourceList("", "")))
 	err := handler.Validate(admission.NewAttributesRecord(oldPod, nil, api.Kind("Pod").WithVersion("version"), oldPod.Namespace, oldPod.Name, api.Resource("pods").WithVersion("version"), "", admission.Create, nil))
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
 
-	kubeClient, handler = SetupQuotaforCreateAndUpdate("1200m", "60Gi", stopCh)
+	kubeClient, handler = SetupQuotaforCreateAndUpdate("1200m", "60Gi", stopCh, 4)
 	newPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("100m", "2Gi"), getResourceList("", "")))
 	newPod.ResourceVersion = "11"
 	err = handler.Validate(admission.NewAttributesRecord(newPod, oldPod, api.Kind("Pod").WithVersion("version"), newPod.Namespace, newPod.Name, api.Resource("pods").WithVersion("version"), "", admission.Update, nil))
@@ -266,7 +266,7 @@ func TestAdmitBelowQuotaLimitWithDecreasingResourceUpdate(t *testing.T) {
 			Used: api.ResourceList{
 				api.ResourceCPU:    resource.MustParse("1100m"),
 				api.ResourceMemory: resource.MustParse("52Gi"),
-				api.ResourcePods:   resource.MustParse("3"),
+				api.ResourcePods:   resource.MustParse("4"),
 			},
 		},
 	}
@@ -284,7 +284,7 @@ func TestAdmitBelowQuotaLimitWithIncreasingResourceUpdate(t *testing.T) {
 	utilfeaturetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.VerticalScaling, true)
 	stopCh := make(chan struct{})
 	defer close(stopCh)
-	kubeClient, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh)
+	kubeClient, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh, 3)
 	oldPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("100m", "2Gi"), getResourceList("", "")))
 	oldPod.ResourceVersion = "10"
 	err := handler.Validate(admission.NewAttributesRecord(oldPod, nil, api.Kind("Pod").WithVersion("version"), oldPod.Namespace, oldPod.Name, api.Resource("pods").WithVersion("version"), "", admission.Create, nil))
@@ -293,7 +293,7 @@ func TestAdmitBelowQuotaLimitWithIncreasingResourceUpdate(t *testing.T) {
 	}
 
 	// update the usage to account for the first update
-	kubeClient, handler = SetupQuotaforCreateAndUpdate("1100m", "52Gi", stopCh)
+	kubeClient, handler = SetupQuotaforCreateAndUpdate("1100m", "52Gi", stopCh, 4)
 
 	newPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("200m", "10Gi"), getResourceList("", "")))
 	newPod.ResourceVersion = "11"
@@ -315,7 +315,7 @@ func TestAdmitBelowQuotaLimitWithIncreasingResourceUpdate(t *testing.T) {
 			Used: api.ResourceList{
 				api.ResourceCPU:    resource.MustParse("1200m"),
 				api.ResourceMemory: resource.MustParse("60Gi"),
-				api.ResourcePods:   resource.MustParse("3"),
+				api.ResourcePods:   resource.MustParse("4"),
 			},
 		},
 	}
@@ -333,14 +333,14 @@ func TestAdmitExceedsQuotaLimitWithUpdate(t *testing.T) {
 	utilfeaturetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.VerticalScaling, true)
 	stopCh := make(chan struct{})
 	defer close(stopCh)
-	_, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh)
+	_, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh, 3)
 	oldPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("100m", "2Gi"), getResourceList("", "")))
 	err := handler.Validate(admission.NewAttributesRecord(oldPod, nil, api.Kind("Pod").WithVersion("version"), oldPod.Namespace, oldPod.Name, api.Resource("pods").WithVersion("version"), "", admission.Create, nil))
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
 	// update the usage to account for the first update
-	_, handler = SetupQuotaforCreateAndUpdate("1100m", "52Gi", stopCh)
+	_, handler = SetupQuotaforCreateAndUpdate("1100m", "52Gi", stopCh, 4)
 
 	newPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("200m", "100Gi"), getResourceList("", "")))
 	expectedErr := "pods \"allowed-pod\" is forbidden: exceeded quota: quota, requested: memory=98Gi, used: memory=52Gi, limited: memory=100Gi"
@@ -353,9 +353,10 @@ func TestAdmitExceedsQuotaLimitWithUpdate(t *testing.T) {
 
 // TestAdmitBelowQuotaLimit verifies that a pod when created has its usage reflected on the quota
 func TestAdmitBelowQuotaLimit(t *testing.T) {
+	utilfeaturetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.VerticalScaling, false)
 	stopCh := make(chan struct{})
 	defer close(stopCh)
-	kubeClient, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh)
+	kubeClient, handler := SetupQuotaforCreateAndUpdate("1", "50Gi", stopCh, 3)
 	newPod := validPod("allowed-pod", 1, getResourceRequirements(getResourceList("100m", "2Gi"), getResourceList("", "")))
 	err := handler.Validate(admission.NewAttributesRecord(newPod, nil, api.Kind("Pod").WithVersion("version"), newPod.Namespace, newPod.Name, api.Resource("pods").WithVersion("version"), "", admission.Create, nil))
 	if err != nil {
